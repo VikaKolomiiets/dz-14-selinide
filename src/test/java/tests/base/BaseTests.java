0f04aad13baa4a0976f886c8d54f1367cfbf77c8
Vika Kolomiets <vikakolomiets@gmail.com>
2023-06-26 20:24:51 +0300

    updated BaseTests.java

--- src/test/java/tests/base/BaseTests.java
+++ src/test/java/tests/base/BaseTests.java
@@ -2,21 +2,16 @@ package tests.base;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import pages.BasePage;
 
 public class BaseTests {
 
     @BeforeClass
     public void setUpClass(){
         Configuration.browser = "chrome";
-        Configuration.baseUrl = "https://demoqa.com/";
+        Configuration.browserSize="1024x768";
     }
 
     @BeforeMethod
